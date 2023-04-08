0c319b5926d185f5b69bd5cf435730a281daa814
Mikołaj Sieńko <mikolaj.sienko@esgroup.ch>
2023-04-08 15:26:46 +0200

    Sidebar styling

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,35 +1,17 @@
 import { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
-import { Box, IconButton, Typography, useTheme } from "@mui/material";
+import { Box, IconButton, Tooltip, Typography, useTheme } from "@mui/material";
 import { Link } from "react-router-dom";
 import "react-pro-sidebar/dist/css/styles.css";
 import { tokens } from "../../theme";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
-import CasesOutlinedIcon from '@mui/icons-material/CasesOutlined';
+import CasesOutlinedIcon from "@mui/icons-material/CasesOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
-import TaskIcon from '@mui/icons-material/Task';
-import ApartmentOutlinedIcon from '@mui/icons-material/ApartmentOutlined';
-import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
-
-const Item = ({ title, to, icon, selected, setSelected }) => {
-  const theme = useTheme();
-  const colors = tokens(theme.palette.mode);
-  return (
-    <MenuItem
-      active={selected === title}
-      style={{
-        color: colors.grey[100],
-      }}
-      onClick={() => setSelected(title)}
-      icon={icon}
-    >
-      <Typography>{title}</Typography>
-      <Link to={to} />
-    </MenuItem>
-  );
-};
+import TaskIcon from "@mui/icons-material/Task";
+import ApartmentOutlinedIcon from "@mui/icons-material/ApartmentOutlined";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 
 const Sidebar = () => {
   const theme = useTheme();
@@ -37,8 +19,33 @@ const Sidebar = () => {
   const [isCollapsed, setIsCollapsed] = useState(false);
   const [selected, setSelected] = useState("Dashboard");
 
+  const Item = ({ title, to, icon, selected, setSelected }) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+      <MenuItem
+        active={selected === title}
+        style={{
+          color: colors.grey[100],
+        }}
+        onClick={() => setSelected(title)}
+        icon={icon}
+      >
+        {!isCollapsed && 
+        <div>
+          <Typography>{title}</Typography>
+        </div>
+        }
+        <Tooltip title={title}>
+          <Link to={to} />
+        </Tooltip>
+      </MenuItem>
+    );
+  };
+
   return (
     <Box
+      // position="static"
       sx={{
         "& .pro-sidebar-inner": {
           background: `${colors.primary[400]} !important`,
@@ -47,7 +54,8 @@ const Sidebar = () => {
           backgroundColor: "transparent !important",
         },
         "& .pro-inner-item": {
-          // padding: "5px 35px 5px 20px !important",
+          padding: "8px 20px 8px 20px !important"
+          // padding: "0px 10px 16px 20px !important",
         },
         "& .pro-inner-item:hover": {
           color: "#868dfb !important",
@@ -58,13 +66,14 @@ const Sidebar = () => {
       }}
     >
       <ProSidebar collapsed={isCollapsed}>
-        <Menu iconShape="square">
+        <Menu iconShape="rectangle">
           {/* LOGO AND MENU ICON */}
           <MenuItem
             onClick={() => setIsCollapsed(!isCollapsed)}
             icon={isCollapsed ? <MenuOutlinedIcon /> : undefined}
             style={{
-              margin: "10px 0 20px 0px",
+              // margin: "10px 0 20px 0px",
+              margin : "0",
               color: colors.grey[100],
             }}
           >
@@ -72,12 +81,18 @@ const Sidebar = () => {
               <Box
                 display="flex"
                 justifyContent="space-between"
-                alignItems="center"
-                ml="10px"
+                // alignItems="left"
+                ml="5px"
               >
-                <Typography variant="h3" color={colors.grey[100]}>
-                  ASON Portal
-                </Typography>
+                <Box display="flex" justifyContent="left" alignItems="left">
+                  <img
+                    alt="profile-user"
+                    width="100px"
+                    src={`../../assets/logo/logo.png`}
+                    style={{ cursor: "pointer" }}
+                  />
+                </Box>
+
                 <IconButton onClick={() => setIsCollapsed(!isCollapsed)}>
                   <MenuOutlinedIcon />
                 </IconButton>
@@ -97,7 +112,7 @@ const Sidebar = () => {
             <Typography
               variant="h5"
               color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 0px" }}
+              sx={{ m: "10px 0" }}
               textAlign="center"
             >
               Employee
@@ -128,7 +143,7 @@ const Sidebar = () => {
             <Typography
               variant="h5"
               color={colors.grey[300]}
-              sx={{ m: "15px 0 5px 0px" }}
+              sx={{ m: "10px 0" }}
               textAlign="center"
             >
               HR
@@ -177,8 +192,8 @@ const Sidebar = () => {
             /> */}
           </Box>
 
-          {!isCollapsed && (
-            <Box mt="180px">
+          {/* {!isCollapsed && (
+            <Box mt="100px">
               <Box display="flex" justifyContent="left" alignItems="left" marginLeft="35px">
                 <img
                   alt="profile-user"
@@ -202,7 +217,7 @@ const Sidebar = () => {
                 </Typography>
               </Box>
             </Box>
-          )}
+          )} */}
         </Menu>
       </ProSidebar>
     </Box>
--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -13,59 +13,33 @@ const Topbar = () => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
   const colorMode = useContext(ColorModeContext);
-  const [cases, setCases] = useState([]); // TODO: implement logic for the autocomplete top bar search
-
-  const callbackDropdown = function (error, data, response) {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(
-        "API called successfully. Returned Dropdown data: " +
-          JSON.stringify(data, null, 2)
-      );
-    }
-    setCases(data[0].values);
-    console.log(
-      "Dropdowns: " +
-        JSON.stringify(data[0].values, null, 2)
-    );
-  };
 
   return (
-    <Box display="flex" justifyContent="space-between" p={3}>
-      {/* SEARCH BAR */}
-      <Box
-        display="flex"
-        backgroundColor={colors.primary[400]}
-        borderRadius="3px"
-      >
-        <InputBase sx={{ ml: 2, mr: 1, flex: 1, width: "250px" }} 
-        placeholder="What do you want to report?" 
-        // placeholder="What Case do you want to report?" 
-        
-        options={cases}
-        // renderInput={(params) => <TextField {...params} label="Movie" />}
-        />
-        <IconButton type="button" sx={{ p: 1 }}>
-          <SearchIcon />
-        </IconButton>
-      </Box>
-
+    <Box 
+      display="flex" 
+      flexDirection="row-reverse" 
+      justifyContent="space-between" 
+      padding="10px 6px 0px">
       {/* ICONS */}
       <Box display="flex">
-        <IconButton onClick={colorMode.toggleColorMode}>
+        <IconButton 
+          onClick={colorMode.toggleColorMode}
+          // size="large"
+          >
           {theme.palette.mode === "dark" ? (
             <DarkModeOutlinedIcon />
           ) : (
             <LightModeOutlinedIcon />
           )}
         </IconButton>
-        <IconButton>
+
+        {/* <IconButton>
           <NotificationsOutlinedIcon />
         </IconButton>
         <IconButton>
           <SettingsOutlinedIcon />
-        </IconButton>
+        </IconButton> */}
+        
         <IconButton>
           <PersonOutlinedIcon />
         </IconButton>
