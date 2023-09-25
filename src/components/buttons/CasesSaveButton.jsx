194d62bae6cf0dbf86b88e6bd32ba2fbc9907c90
Alex <143416756+ason-ajo@users.noreply.github.com>
2023-09-25 11:41:55 +0200

    disable submit button when processing addcase

--- src/components/buttons/CasesSaveButton.jsx
+++ src/components/buttons/CasesSaveButton.jsx
@@ -1,16 +1,32 @@
 import SendIcon from "@mui/icons-material/Send";
+import { CircularProgress } from "@mui/material";
 import { Button, Typography } from "@mui/material";
+import { useNavigation } from "react-router-dom";
+
+const iconProps = {
+  size: "1em",
+  sx: {
+    color: "common.white"
+  }
+};
 
 export default function CasesSaveButton({ onSubmit }) {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting" && navigation.json?.intent === "addCase";
+  const isRedirecting = navigation.state === "loading" && navigation.json && navigation.formAction !== navigation.location.pathname;
+  const isProcessing = isSubmitting || isRedirecting;
+  const icon = isProcessing ? <CircularProgress {...iconProps} /> : <SendIcon {...iconProps} />;
+
   return (
     <Button
+      disabled={isProcessing}
       disableRipple
       type="button"
       variant="contained"
       color="primary"
       size="large"
       onClick={onSubmit}
-      endIcon={<SendIcon htmlColor="#ffffff" />}
+      endIcon={icon}
     >
       <Typography fontWeight="bold" color="#ffffff">
         Save
