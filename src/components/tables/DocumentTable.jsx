95ad89b83613623d9924c8f5c61aa9bd9e887318
Alex <143416756+ason-ajo@users.noreply.github.com>
2023-10-03 17:15:25 +0200

    redesign document table

--- src/components/tables/DocumentTable.jsx
+++ src/components/tables/DocumentTable.jsx
@@ -1,107 +1,157 @@
 import {
-  Backdrop,
-  Box,
-  CircularProgress,
+  Chip,
+  Divider,
+  Dialog,
   Stack,
   Typography,
-  Button
+  Box,
+  DialogTitle,
+  DialogContent,
+  IconButton,
+  Button,
+  Slide
 } from "@mui/material";
-import { React, useState } from "react";
-import TableView from "./TableView";
-import { FileIcon, defaultStyles } from "react-file-icon";
-import { useOutletContext, useParams } from "react-router-dom";
+import { React, useEffect, useState, Suspense } from "react";
+import { useOutletContext, useParams, useAsyncValue, useLoaderData, Await } from "react-router-dom";
 import { getDocument } from "../../api/FetchClient";
-import { createDateColumns } from "./caseValueDateColumns";
 import { useTranslation } from "react-i18next";
+import Header from "../Header";
+import { Loading } from "../Loading";
+import { ErrorView } from "../ErrorView";
+import dayjs from "dayjs";
+import { Close } from "@mui/icons-material";
+import { Centered } from "../Centered";
+import { useTheme } from "@emotion/react";
+import { useIsMobile } from "../../hooks/useIsMobile";
 
-
-export function DocumentTable({ defaultTitle }) {
+export function DocumentTableRoute({defaultTitle}) {
+  const routeData = useLoaderData();
   const title = useOutletContext() || defaultTitle;
-  const params = useParams();
-  const [backdropOpen, setBackdropOpen] = useState(false);
   const { t } = useTranslation();
+  return (
+    <Stack p={4} gap={2} useFlexGap sx={{height: "100%"}}>
+      <Header title={t(title)} />
+      <Suspense fallback={<Loading />}>
+        <Await resolve={routeData.data} errorElement={<ErrorView />}>
+          <DocumentTable />
+        </Await>
+      </Suspense>
+    </Stack>
+  );
+}
 
-  const handleFileClick = async (documentId, caseValueId) => {
-    setBackdropOpen(true);
-    try {
-      const document = await getDocument(params.tenantId, caseValueId, documentId, params.employeeId);
-      downloadBase64File(document.content, document.contentType, document.name);
-    }
-    finally {
-      setBackdropOpen(false);
-    }
-  };
 
-  const columns = [
-    {
-      field: "caseFieldName",
-      headerName: t("Name (event field name)"),
-      flex: 2,
-    },
-    {
-      field: "documents",
-      headerName: t("Documents"),
-      flex: 3,
-      renderCell: ({ row: caseValue }) => {
-        if (Array.isArray(caseValue.documents)) {
-          return (
-            <Stack>
-              {caseValue.documents.map((doc) => {
-                const extension = doc.name?.split(".").pop();
-                return (
-                  <Stack
-                    sx={{py: 1}}
-                    direction="row"
-                    spacing={1}
-                    onClick={() => handleFileClick(doc.id, caseValue.id)}
-                    component={Button}
-                    alignItems="center"
-                    key={doc.id}
-                  >
-                    <Box width="24px">
-                      <FileIcon extension={extension} {...defaultStyles[extension]}/>
-                    </Box>
-                    <Typography>{doc.name}</Typography>
-                  </Stack>
-                );
-              })}
-            </Stack>
-          );
-        }
-      },
-    },
-    ...createDateColumns()
-  ];
+export function DocumentTable() {
+  const caseValues = useAsyncValue();
+  const [selectedDocument, setSelectedDocument] = useState(null);
 
   return (
-    <>
-      <TableView
-        title={t(title)}
-        columns={columns}
-        initialState={{
-          sorting: {
-            sortModel: [{ field: "created", sort: "desc" }],
-          },
-        }}
-        getRowHeight={() => "auto"}
-      />
-      <Backdrop
-        sx={{
-          color: "#fff",
-          zIndex: (theme) => theme.zIndex.drawer + 1,
-        }}
-        open={backdropOpen}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
-    </>
+    <Stack spacing={3} divider={<Divider />}>
+      {caseValues.map(caseValue => <CaseValueRow key={caseValue.id} caseValue={caseValue} onSelect={setSelectedDocument} />)}
+      <DocumentView {...selectedDocument} onClose={() => setSelectedDocument(null)} />
+    </Stack>
   );
 };
 
-function downloadBase64File(base64Data, contentType, fileName) {
-  const linkSource = `data:${contentType};base64,${base64Data}`;
-  const downloadLink = document.createElement("a");
-  downloadLink.href = linkSource;
-  downloadLink.download = fileName;
-  downloadLink.click();
+const style = {
+  whiteSpace: "nowrap",
+  textOverflow: "ellipsis",
+  overflow: "hidden"
+};
+
+function CaseValueRow({ caseValue, onSelect }) {
+  const created = dayjs.utc(caseValue.created).format("L LT")
+  const { t } = useTranslation();
+  return (
+    <Stack direction="row" spacing={1.5}>
+      <Stack flex={1} flexShrink={0}>
+        <Typography variant="h6" component="div" sx={style}>{caseValue.caseFieldName}</Typography>
+        <Typography variant="body2" sx={style}>{t("Created")}: {created}</Typography>
+      </Stack>
+      <Stack direction="row" spacing={1} useFlexGap justifyContent="end" flexWrap="wrap" alignItems="center">
+      {
+        caseValue.documents.map(doc => <Chip key={doc.id} label={doc.name} color="primary" variant="outlined" onClick={() => onSelect({caseValueId: caseValue.id, documentRef: doc})} />)
+      }
+      </Stack>
+    </Stack>
+  )
+}
+
+function getDialogDesktopProps(open) {
+  return {
+    open,
+    fullWidth: true,
+    maxWidth: "lg",
+    PaperProps: {
+      sx: { minHeight: "90vh" }
+    }
+  }
+}
+
+function getDialogMobileProps(open) {
+  return {
+    open,
+    fullScreen: true,
+    TransitionComponent: Slide,
+    TransitionProps: {
+      direction: "up",
+      in: open
+    }
+  };
+}
+
+function DocumentView({ caseValueId, documentRef, onClose }) {
+  const params = useParams();
+  const [documentDataUrl, setDocumentDataUrl] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const open = !!documentRef;
+  const isMobile = useIsMobile();
+
+  useEffect(() => {
+    const loadData = async () => {
+      setLoading(true);
+      try {
+        const doc = await getDocument({caseValueId, documentId: documentRef?.id, ...params});
+        setDocumentDataUrl(getDataUrl(doc));
+      }
+      catch {
+      }
+      finally {
+        setLoading(false);
+      }
+      
+    }
+    loadData();
+  }, [caseValueId, documentRef?.id, params]);
+
+  const dialogContent = isLoading ?
+    <Loading /> :
+    (<Box sx={{minHeight: "100%", width: "100%", border: 'none'}} component="object" data={documentDataUrl} type={documentRef?.contentType}>
+      <Centered><Typography>Unable to display document. Tap the button to download it.</Typography></Centered>
+    </Box>);
+
+  const dialogProps = (isMobile ? getDialogMobileProps : getDialogDesktopProps)(open);
+
+  return (
+    <Dialog onClose={onClose} {...dialogProps}>
+      <Stack direction="row" alignItems="center" pr={2} spacing={2}>
+        <DialogTitle sx={{flex: 1}}>{documentRef?.name}</DialogTitle>
+        <DownloadButton url={documentDataUrl} name={documentRef?.name} />
+        <IconButton onClick={onClose}>
+          <Close />
+        </IconButton>
+      </Stack>
+      <DialogContent dividers sx={{display: 'flex', py: 3}}>
+        {dialogContent}
+      </DialogContent>  
+    </Dialog>
+  )
+}
+
+function DownloadButton({url, name}) {
+  return <Button component="a" variant="outlined" href={url} download={name}>Download</Button>
+}
+
+function getDataUrl(document) {
+  return `data:${document.contentType};base64,${document.content}`
 }
--- src/components/tables/EmployeeRow.jsx
+++ src/components/tables/EmployeeRow.jsx
@@ -5,45 +5,46 @@ import styled from "@emotion/styled";
 import { forwardRef } from "react";
 
 const Link = styled(forwardRef(function Link(itemProps, ref) {
-    return <RouterLink ref={ref} {...itemProps} role={undefined} />;
-  }))(({theme}) => {
-    return {
-      display: "block",
-      textDecoration: "none",
-      color: theme.palette.text.primary,
-      flex: 1,
-      "&:hover": {
-        "color": theme.palette.primary.main,
-      },
-      "&.active": {
-        "color": theme.palette.primary.main
-      },
-      "&.active:hover": {
-        "color": theme.palette.primary.light,
-      }
-    }
-  });
-
-const sx = {
-    borderRadius: (theme) => theme.spacing(1),
-    padding: (theme) => theme.spacing(1),
+  return <RouterLink ref={ref} {...itemProps} role={undefined} />;
+}))(({theme}) => {
+  return {
+    display: "block",
+    textDecoration: "none",
+    color: theme.palette.text.primary,
+    flex: 1,
     "&:hover": {
-      "backgroundColor": (theme) => theme.palette.primary.hover
+      "color": theme.palette.primary.main,
     },
     "&.active": {
-      "backgroundColor": (theme) => theme.palette.primary.active
+      "color": theme.palette.primary.main
+    },
+    "&.active:hover": {
+      "color": theme.palette.primary.light,
     }
+  }
+});
+
+const sx = {
+  borderRadius: (theme) => theme.spacing(1),
+  padding: (theme) => theme.spacing(1),
+  "&:hover": {
+    "backgroundColor": (theme) => theme.palette.primary.hover
+  },
+  "&.active": {
+    "backgroundColor": (theme) => theme.palette.primary.active
+  }
 }
 
 export function EmployeeRow({ employee, variant }) {
-    return <Stack direction="row" alignItems="center" gap={2} marginX={-1} sx={sx}>
-            <Link to={employee.id + ""}>
-                <Stack>
-                    <Typography gutterBottom>{employee.firstName}&nbsp;{employee.lastName}</Typography>
-                    <Typography variant="body2" sx={{textOverflow: 'ellipsis', overflow: 'hidden'}}>{employee.identifier}</Typography>
-                </Stack>
-            </Link>
-            <EmployeeButtons employeeId={employee.id} variant={variant} />
+  return (
+    <Stack direction="row" alignItems="center" gap={2} marginX={-1} sx={sx}>
+      <Link to={employee.id + ""}>
+        <Stack>
+          <Typography gutterBottom>{employee.firstName}&nbsp;{employee.lastName}</Typography>
+          <Typography variant="body2" sx={{textOverflow: 'ellipsis', overflow: 'hidden'}}>{employee.identifier}</Typography>
         </Stack>
-    
+      </Link>
+      <EmployeeButtons employeeId={employee.id} variant={variant} />
+    </Stack>
+  );
 }
