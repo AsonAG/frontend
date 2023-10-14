1886b1ccb171924562bb1ce4eef08e745945530c
Alex <143416756+ason-ajo@users.noreply.github.com>
2023-10-14 15:41:44 +0200

    fix casedata header when no data is available

--- src/routes/AsyncDataRoute.jsx
+++ src/routes/AsyncDataRoute.jsx
@@ -5,11 +5,12 @@ import { Await, useLoaderData } from "react-router-dom";
 import { Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
-export function AsyncDataRoute({ children }) {
+export function AsyncDataRoute({ children, loadingElement }) {
   const routeData = useLoaderData();
   const { t } = useTranslation();
+  const loading = loadingElement || <Loading />;
   return (
-    <Suspense fallback={<Loading />}>
+    <Suspense fallback={loading}>
       <Await resolve={routeData.data} errorElement={<ErrorView />}>
         {(data) => hasData(data) ? children : <Typography>{t("No data available")}</Typography>}
       </Await>
