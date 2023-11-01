3408cfa7290e09e25997406b647b4214b3b0a7f1
Alex <143416756+ason-ajo@users.noreply.github.com>
2023-11-01 16:15:45 +0100

    add word break to instructions

--- src/components/HtmlContent.jsx
+++ src/components/HtmlContent.jsx
@@ -1,7 +1,12 @@
+import { Box } from "@mui/material";
 import { Markup } from "interweave";
 
 const isHtml = RegExp.prototype.test.bind(/<\/?[a-z][\s\S]*>/i);
 
 export function HtmlContent({content}) {
-  return isHtml(content) ? <Markup content={content} /> : content;
+  return (
+    <Box sx={{wordBreak: "break-word"}}>
+      {isHtml(content) ? <Markup content={content} /> : content}
+    </Box>
+  )
 }
