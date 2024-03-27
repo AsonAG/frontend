457fccdadbf75fe9ef550ca372ce273393fd3da9
Aleksandar Josipovic <ajo@ason.ch>
2024-03-27 18:24:47 +0100

    add link on employee name

--- src/components/tables/EmployeeTable.jsx
+++ src/components/tables/EmployeeTable.jsx
@@ -1,10 +1,11 @@
-import { React, createContext, useContext, useState } from "react";
-import { useAsyncValue } from "react-router-dom";
+import { React, createContext, useContext, useState, forwardRef } from "react";
+import { useAsyncValue, Link as RouterLink } from "react-router-dom";
 import { Divider, Stack, Typography, TextField, useMediaQuery, InputAdornment, IconButton, Button } from "@mui/material";
 import { useTheme } from "@emotion/react";
 import { TableButton } from "../buttons/TableButton";
 import { useTranslation } from "react-i18next";
 import { useDebounceCallback } from "usehooks-ts";
+import styled from "@emotion/styled";
 
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 import WorkHistoryOutlinedIcon from "@mui/icons-material/WorkHistoryOutlined";
@@ -33,6 +34,30 @@ export function AsyncEmployeeTable() {
     </VariantContext.Provider>
   );
 }
+
+const Link = styled(forwardRef(function Link(itemProps, ref) {
+  return <RouterLink ref={ref} {...itemProps} role={undefined} />;
+}))(({theme}) => {
+  return {
+    display: "flex",
+    textDecoration: "none",
+    borderRadius: theme.spacing(0.5),
+    color: theme.palette.text.primary,
+    alignSelf: "stretch",
+    alignItems: "center",
+    flex: 1,
+    "&:hover": {
+      "color": theme.palette.primary.main,
+    },
+    "&.active": {
+      "color": theme.palette.primary.main,
+    },
+    "&.active:hover": {
+      "color": theme.palette.primary.light,
+    }
+  }
+});
+
 
 function EmployeeTableButtons() {
   const { t } = useTranslation();
@@ -123,14 +148,13 @@ function EmployeeTable() {
   const employees = useAsyncValue();
   return <>
     <Stack spacing={1} divider={<Divider />}>
-      {employees.map((employee, index) => <EmployeeRow key={index} employee={employee} />)}
+      {employees.map((employee) => <EmployeeRow key={employee.id} employee={employee} />)}
     </Stack>
   </>;
 };
 
 const sx = {
   borderRadius: (theme) => theme.spacing(1),
-  padding: (theme) => theme.spacing(1),
   "&:hover": {
     "backgroundColor": (theme) => theme.palette.primary.hover
   },
@@ -142,11 +166,13 @@ const sx = {
 function EmployeeRow({ employee }) {
   const variant = useContext(VariantContext);
   return (
-    <Stack direction="row" alignItems="center" spacing={2} ml={-2} mr={-1} sx={sx}>
-      <Stack flex={1} p={1} direction="row" spacing={1} flexWrap="wrap">
-        <Typography>{employee.firstName} {employee.lastName}</Typography>
-        <Typography color="text.secondary" sx={{textOverflow: 'ellipsis', overflow: 'hidden'}}>{employee.identifier}</Typography>
-      </Stack>
+    <Stack direction="row" alignItems="center" sx={sx} mx={-0.5} px={0.5}>
+      <Link to={`${employee.id + ""}/new`}>
+        <Stack direction="row" spacing={1} flexWrap="wrap">
+          <Typography>{employee.firstName} {employee.lastName}</Typography>
+          <Typography color="text.secondary" sx={{textOverflow: 'ellipsis', overflow: 'hidden'}}>{employee.identifier}</Typography>
+        </Stack>
+      </Link>
       {
         variant === "standard" &&
           <EmployeeButtons employeeId={employee.id} />
@@ -160,7 +186,7 @@ function EmployeeButtons ({ employeeId }) {
   const variant = "dense";
 
   return (
-    <Stack direction="row" spacing={2}>
+    <Stack direction="row" spacing={2} py={0.5}>
       <TableButton title={t("New event")} to={employeeId + "/new"} variant={variant} icon={<AddOutlinedIcon />} />
       <TableButton title={t("Events")} to={employeeId + "/events"} variant={variant} icon={<WorkHistoryOutlinedIcon />} />
       <TableButton title={t("Documents")} to={employeeId + "/documents"} variant={variant} icon={<DescriptionOutlinedIcon />} />
