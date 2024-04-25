c6e3c4aa2d0915fcce764de2e7f79672d634da91
Aleksandar Josipovic <ajo@ason.ch>
2024-04-25 06:14:03 +0200

    fix current search params

--- src/hooks/useSearchParam.tsx
+++ src/hooks/useSearchParam.tsx
@@ -5,8 +5,8 @@ import { NavigateOptions, useSearchParams } from "react-router-dom";
 export function useSearchParam(name: string, opts: NavigateOptions | undefined) {
   const [searchParams, setSearchParams] = useSearchParams();
   const value = searchParams.get(name) || "";
-  const setValue = (updatedValue: string) => setSearchParams(prev => {
-    const searchParams = new URLSearchParams(prev);
+  const setValue = (updatedValue: string) => setSearchParams(() => {
+    let searchParams = new URL(document.location.toString()).searchParams;
     if (updatedValue) {
       searchParams.set(name, updatedValue);
     } else {
