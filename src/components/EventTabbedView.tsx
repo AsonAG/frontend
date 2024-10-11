0c151e90cf75f2b955e3aac1ed029aeaff2513fd
Aleksandar Josipovic <ajo@ason.ch>
2024-10-11 15:03:46 +0200

    switch data / events tab

--- src/components/EventTabbedView.tsx
+++ src/components/EventTabbedView.tsx
@@ -43,8 +43,8 @@ export function EventTabbedView({ title, showMissingData }: { title: ReactNode, 
         !renderTitleOnly &&
         <Stack direction="row" spacing={2} flexWrap="wrap">
           {isMobile && <TabLink title={t("New Event")} to="new" />}
-          <TabLink title={t("Events")} to="events" />
           <TabLink title={t("Data")} to="data" />
+          <TabLink title={t("Events")} to="events" />
           <TabLink title={t("Documents")} to="documents" />
           {showMissingData && missingDataCount > 0 && (
             <TabLink
