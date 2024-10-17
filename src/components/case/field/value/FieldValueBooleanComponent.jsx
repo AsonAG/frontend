097333fae6d93dfa6e371d07e9cfe7329a5075cf
Aleksandar Josipovic <ajo@ason.ch>
2024-10-17 13:16:00 +0200

    input.switch control for booleans

--- src/components/case/field/value/FieldValueBooleanComponent.jsx
+++ src/components/case/field/value/FieldValueBooleanComponent.jsx
@@ -1,6 +1,7 @@
-import { FormControl, FormControlLabel, Checkbox } from "@mui/material";
+import { FormControl, FormControlLabel, Checkbox, Switch as MuiSwitch, styled, Box } from "@mui/material";
 import { useContext } from "react";
 import { FieldContext } from "../Field";
+import { useTranslation } from "react-i18next";
 
 export function FieldValueBooleanComponent() {
 	const { field, isReadonly, displayName, buildCase } =
@@ -14,6 +15,7 @@ export function FieldValueBooleanComponent() {
 		buildCase();
 	}
 
+	const Control = field.attributes?.["input.switch"] ? Switch : Checkbox;
 	return (
 		<FormControl sx={{ flex: 1 }}>
 			<FormControlLabel
@@ -21,7 +23,7 @@ export function FieldValueBooleanComponent() {
 				label={displayName}
 				labelPlacement="end"
 				control={
-					<Checkbox
+					<Control
 						checked={checked}
 						onChange={handleChange}
 						disabled={isReadonly}
@@ -31,3 +33,83 @@ export function FieldValueBooleanComponent() {
 		</FormControl>
 	);
 }
+
+function Switch({ checked, disabled, ...props }) {
+	const { t } = useTranslation();
+	return (
+		<Box mx={1.25}>
+			<LabelSwitch
+				checked={checked}
+				disabled={disabled}
+				disableRipple
+				{...props}
+			/>
+			<Box sx={disabled ? labelContainerDisabledSx : labelContainerSx}>
+				<div>{checked ? '' : t("No")}</div>
+				<div>{checked ? t("Yes") : ''}</div>
+			</Box>
+		</Box>)
+}
+
+
+const LabelSwitch = styled(MuiSwitch)(({ theme, disabled }) => ({
+	height: 22,
+	padding: 0,
+	position: 'relative',
+	'& .MuiSwitch-switchBase': {
+		padding: 0,
+		margin: 2,
+		transitionDuration: '300ms',
+		'&.Mui-checked': {
+			transform: 'translateX(36px)',
+			'& + .MuiSwitch-track': {
+				backgroundColor: disabled ? theme.palette.action.disabledBackground : theme.palette.primary.main,
+				opacity: 1,
+				border: 0,
+			},
+		},
+	},
+	'& .MuiSwitch-thumb': {
+		backgroundColor: disabled ? theme.palette.action.disabled : theme.palette.common.white,
+		width: 18,
+		height: 18,
+		borderRadius: "50%",
+		position: 'relative',
+		zIndex: 1,
+	},
+	'& .MuiSwitch-track': {
+		borderRadius: 30,
+		backgroundColor: disabled ? theme.palette.action.disabledBackground : `rgba(${theme.vars.palette.common.onBackgroundChannel} / 0.35)`,
+		opacity: "1 !important",
+		transition: theme.transitions.create(['background-color'], {
+			duration: 300,
+		}),
+		position: 'relative',
+	}
+}));
+
+const labelContainerSx = {
+	"--text-color": theme => theme.palette.common.white,
+	"& > *": {
+		position: 'absolute',
+		px: 1,
+		width: 58,
+		display: 'flex',
+		top: '50%',
+		transform: 'translateY(-50%)',
+		fontSize: '12px',
+		color: 'var(--text-color)',
+		pointerEvents: 'none'
+	},
+	"& :first-child": {
+		justifyContent: 'end'
+	},
+	"& :last-child": {
+		justifyContent: 'start'
+	}
+};
+
+const labelContainerDisabledSx = {
+	...labelContainerSx,
+	"--text-color": theme => theme.palette.action.disabled
+};
