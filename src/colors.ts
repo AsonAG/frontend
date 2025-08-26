9e6ba675e7dbc8b921c66913b95c710d2af71e38
Alexander Prinzhorn <alexander@prinzhorn.it>
2025-08-26 09:21:38 +0200

    Extract colors

---
+++ src/colors.ts
@@ -0,0 +1,11 @@
+export const lightColors = {
+  primary: "#461eb7",
+  selection: "#e3ddf4",
+  selectionAttention: "#fdf0e5"
+};
+
+export const darkColors = {
+  primary: "#4985e2",
+  selection: "#1a2331",
+  selectionAttention: "#352c1f"
+};
